190dd7f61ba2d766f036556607392b1c4104a64b
Elliott Coleman <ejaycoleman@googlemail.com>
2022-12-06 20:01:02 +0000

    listen, accept, serve

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,133 @@
-use deno_core::op;
-use deno_core::Extension;
-use std::rc::Rc;
+use deno_core::anyhow::Error;
+use deno_core::AsyncRefCell;
+use deno_core::AsyncResult;
+use deno_core::CancelHandle;
+use deno_core::CancelTryFuture;
 use deno_core::error::AnyError;
+use deno_core::Extension;
+use deno_core::op;
+use deno_core::OpState;
+use deno_core::RcRef;
+use deno_core::Resource;
+use deno_core::ResourceId;
+use std::cell::RefCell;
+use deno_core::serde::Serialize;
+use deno_core::serde::Serializer;
+use deno_core::serde::ser::SerializeStruct;
+use std::rc::Rc;
+use std::net::SocketAddr;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+
+struct TcpListener {
+  inner: tokio::net::TcpListener,
+  cancel: CancelHandle,
+}
+
+impl TcpListener {
+  async fn accept(self: Rc<Self>) -> Result<TcpStream, std::io::Error> {
+    let cancel = RcRef::map(&self, |r| &r.cancel);
+    let stream = self.inner.accept().try_or_cancel(cancel).await?.0.into();
+    Ok(stream)
+  }
+}
+
+impl Resource for TcpListener {
+  fn close(self: Rc<Self>) {
+    self.cancel.cancel();
+  }
+}
+
+impl TryFrom<std::net::TcpListener> for TcpListener {
+  type Error = std::io::Error;
+  fn try_from(
+    std_listener: std::net::TcpListener,
+  ) -> Result<Self, Self::Error> {
+    tokio::net::TcpListener::try_from(std_listener).map(|tokio_listener| Self {
+      inner: tokio_listener,
+      cancel: Default::default(),
+    })
+  }
+}
+
+struct TcpStream {
+  rd: AsyncRefCell<tokio::net::tcp::OwnedReadHalf>,
+  wr: AsyncRefCell<tokio::net::tcp::OwnedWriteHalf>,
+  cancel: CancelHandle,
+}
+
+impl TcpStream {
+  async fn read(self: Rc<Self>, data: &mut [u8]) -> Result<usize, Error> {
+    let mut rd = RcRef::map(&self, |r| &r.rd).borrow_mut().await;
+    let cancel = RcRef::map(self, |r| &r.cancel);
+    let nread = rd.read(data).try_or_cancel(cancel).await?;
+    Ok(nread)
+  }
+
+  async fn write(self: Rc<Self>, data: &[u8]) -> Result<usize, Error> {
+    let mut wr = RcRef::map(self, |r| &r.wr).borrow_mut().await;
+    let nwritten = wr.write(data).await?;
+    Ok(nwritten)
+  }
+}
+
+impl Resource for TcpStream {
+  deno_core::impl_readable_byob!();
+  deno_core::impl_writable!();
+
+  fn close(self: Rc<Self>) {
+    self.cancel.cancel()
+  }
+}
+
+impl From<tokio::net::TcpStream> for TcpStream {
+  fn from(s: tokio::net::TcpStream) -> Self {
+    let (rd, wr) = s.into_split();
+    Self {
+      rd: rd.into(),
+      wr: wr.into(),
+      cancel: Default::default(),
+    }
+  }
+}
+
+struct ListenResult{
+  resource_id: ResourceId,
+  port: u64,
+}
+
+impl Serialize for ListenResult {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+      S: Serializer,
+  {
+      let mut s = serializer.serialize_struct("ListenResult", 2)?;
+      s.serialize_field("resourceId", &self.resource_id)?;
+      s.serialize_field("port", &self.port)?;
+      s.end()
+  }
+}
+
+#[op]
+fn op_listen(state: &mut OpState, port: u64) -> Result<ListenResult, Error> {
+  let addr = format!("127.0.0.1:{port}").parse::<SocketAddr>().unwrap();
+  let std_listener = std::net::TcpListener::bind(addr)?;
+  std_listener.set_nonblocking(true)?;
+  let listener = TcpListener::try_from(std_listener)?;
+  let rid = state.resource_table.add(listener);
+  Ok(ListenResult {resource_id: rid, port})
+}
+
+#[op]
+async fn op_accept(
+  state: Rc<RefCell<OpState>>,
+  rid: ResourceId,
+) -> Result<ResourceId, Error> {
+  let listener = state.borrow().resource_table.get::<TcpListener>(rid)?;
+  let stream = listener.accept().await?;
+  let rid = state.borrow_mut().resource_table.add(stream);
+  Ok(rid)
+}
 
 #[op]
 async fn timeout(duration: u64) -> Result<(), AnyError> {
@@ -35,6 +161,8 @@ async fn vehicle(file_path: &str) -> Result<(), AnyError> {
         op_write_file::decl(),
         op_remove_file::decl(),
         timeout::decl(),
+        op_listen::decl(),
+        op_accept::decl()
     ]).build();
     
     // create new JS runtime with file-system based module loader
--- src/runtime.js
+++ src/runtime.js
@@ -17,6 +17,13 @@
     },
   };
 
+  const requestBuf = new Uint8Array(64 * 1024);
+  const responseBuf = new Uint8Array(
+    "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World\n"
+      .split("")
+      .map((c) => c.charCodeAt(0))
+  );
+
   globalThis.vehicle = {
     readFile: (path) => {
       return ops.op_read_file(path);
@@ -30,6 +37,28 @@
     setTimeout: async (callback, timeout) => {
       await ops.timeout(timeout);
       callback();
+    },
+    listen: (port) => {
+      return ops.op_listen(port);
+    },
+    accept: (serverRid) => {
+      return ops.op_accept(serverRid);
+    },
+    serve: async (rid) => {
+      try {
+        while (true) {
+          await core.read(rid, requestBuf);
+          await core.writeAll(rid, responseBuf);
+        }
+      } catch (e) {
+        if (
+          !e.message.includes("Broken pipe") &&
+          !e.message.includes("Connection reset by peer")
+        ) {
+          throw e;
+        }
+      }
+      core.close(rid);
     },
   };
 })(globalThis);
